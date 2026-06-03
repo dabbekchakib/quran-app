685ce0348964f4d978a3edb23631e331a5059e17
dabbekchakib <dabbekchakib@gmail.com>
2026-06-03 10:16:02 +0100

    Custom PWA splash screen: teal bg, white border, round icon

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const hideSplash = () => {
+  const el = document.getElementById('splash');
+  if (el) el.classList.add('hidden');
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+requestAnimationFrame(hideSplash);
